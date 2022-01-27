265b66083c2da6ebb0bcfd073e50c4db2d8d33b8
ahmed <71600486+Ahmed-Aoulad-Amar@users.noreply.github.com>
2022-01-27 10:29:02 +0100

    Add files via upload

---
+++ jquery/trean.js
@@ -0,0 +1,84 @@
+// evants
+// --------------------------------
+$( 'id' ).ready();
+$().click();
+$().on()
+$().keypress()
+// effects
+// --------------------------------
+$().hide(speed, callback);
+$().show(speed, callback);
+$().toggle(speed, callback)
+// --------------------------------
+$().fadeIn(speed, callback);
+$().fadeOut(speed, callback);
+$().fadeToggle(speed, callback); 
+// --------------------------------
+$().slideDown(speed, callback);
+$().slideUp(speed, callback);
+$().slideToggle(speed, callback);
+// --------------------------------
+// UI
+// --------------------------------
+$().draggable();
+$().droppable();
+// --------------------------------
+$().datePicker();
+// --------------------------------
+// STYLING?
+$( 'id' ).css();
+$().addClass(); 
+$().removeClass();
+$().toggleClas();
+
+
+
+
+$( 'id' ).text();
+$().html();
+
+$( 'id' ).val();
+$().attr();
+$().val();
+
+// DOM/
+// SELECT_-------------------------?
+$().find();
+$().children();// one lavel down
+$().closest();//up > dom tree /=> the matching elment
+$().parent();//up > dom tree (x) a single level
+$().next();//following sibling
+$().prev();// = next() (x) up sibling
+$().siblings();//up & down all siblilngs
+$().first();//v
+$().last();//v
+// --------------------------------------?
+$().val();
+$().html();
+$().text();
+  $().before(html)
+  $().after(html);
+  $().append(html);
+  $().prepend(html);
+  $().remove(html);
+$().attr();
+$().removeAttr();//
+$().hasClass();//
+$().addClass();//
+$().removeclass();//
+$().toggleClass();//
+$().prop();//
+// --------------------------------
+  $().clone();// 
+  $().insetAfter();//
+$().insetbefor();//
+  // -----------------------------------
+  $().each();//
+  $().this();//
+  $().eq();//
+  $().filter();//
+  $().has();//
+  $().not();//
+
+
+
